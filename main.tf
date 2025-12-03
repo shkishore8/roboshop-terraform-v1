8c95cab73c9798b582ce090f0de51007c829ca45
kishore babu <shkishore8@gmail.com>
2025-12-03 06:15:49 +0530

    Roboshop frontend is added

--- main.tf
+++ main.tf
@@ -31,3 +31,71 @@ resource "aws_route53_record" "mongodb" {
   ttl     = 300
   records = [aws_instance.mongodb.private_ip]
 }
+
+resource "aws_instance" "catalogue" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-02b07aa1827ae3703"]
+  tags = {
+    Name = "catalogue"
+  }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z04751093FJLNWEV5LUJS"
+  name    = "dev.catalogue"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.catalogue.private_ip]
+}
+
+resource "aws_instance" "redis" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-02b07aa1827ae3703"]
+  tags = {
+    Name = "redis"
+  }
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = "Z04751093FJLNWEV5LUJS"
+  name    = "dev.redis"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.redis.private_ip]
+}
+
+resource "aws_instance" "user" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-02b07aa1827ae3703"]
+  tags = {
+    Name = "user"
+  }
+}
+
+resource "aws_route53_record" "user" {
+  zone_id = "Z04751093FJLNWEV5LUJS"
+  name    = "dev.user"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.user.private_ip]
+}
+
+resource "aws_instance" "cart" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-02b07aa1827ae3703"]
+  tags = {
+    Name = "cart"
+  }
+}
+
+resource "aws_route53_record" "cart" {
+  zone_id = "Z04751093FJLNWEV5LUJS"
+  name    = "dev.cart"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.cart.private_ip]
+}
