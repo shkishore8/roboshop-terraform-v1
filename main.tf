a3e0446e79407cf526dd01506db1d0e4c7ca1f47
kishore babu <shkishore8@gmail.com>
2025-12-03 05:56:06 +0530

    Roboshop frontend is added

--- main.tf
+++ main.tf
@@ -14,3 +14,20 @@ resource "aws_route53_record" "frontend" {
   ttl     = 300
   records = [aws_instance.frontend.private_ip]
 }
+
+resource "aws_instance" "mongodb" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-02b07aa1827ae3703"]
+  tags = {
+    Name = "mongodb"
+  }
+}
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z04751093FJLNWEV5LUJS"
+  name    = "test"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend.private_ip]
+}
